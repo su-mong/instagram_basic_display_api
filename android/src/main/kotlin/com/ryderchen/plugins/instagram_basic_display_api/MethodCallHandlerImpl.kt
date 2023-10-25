6facdf91ba26a1af24fb0e345f016515a4d3e863
su-mong <sumong.woo@ourtube.co.kr>
2023-10-25 12:52:22 +0900

    Fix Error(Type mismatch: inferred type is BinaryMessenger? but BinaryMessenger was expected)

--- android/src/main/kotlin/com/ryderchen/plugins/instagram_basic_display_api/MethodCallHandlerImpl.kt
+++ android/src/main/kotlin/com/ryderchen/plugins/instagram_basic_display_api/MethodCallHandlerImpl.kt
@@ -76,7 +76,7 @@ class MethodCallHandlerImpl(private val instagramBasicDisplayApi: InstagramBasic
         }
     }
 
-    fun startListening(messenger: BinaryMessenger?) {
+    fun startListening(messenger: BinaryMessenger) {
         if (channel != null) {
             Log.wtf(TAG, "Setting a method call handler before the last was disposed.")
             stopListening()
